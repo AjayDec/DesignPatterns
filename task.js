70b18b054805e073d23d63fd609c0ff171a1162b
AjayDec <ajaydec78@gmail.com>
2016-03-11 12:37:05 -0800

    Creational Patterns: Constructor pattern with prototype

--- task.js
+++ task.js
@@ -1,18 +1,17 @@
-//Creational Patterns: Constructor Pattern
+//Creational Patterns: Constructor Pattern, added prototype
 
 var Task = function (name) {
     this.name = name;
     this.completed = false;
-
-    this.complete = function () {
-        console.log('Task completed: ' + this.name);
-        this.completed = true;
-    }
-    this.save = function () {
-        console.log('Saving task: ' + this.name);
-    }
 };
 
+Task.prototype.complete = function () {
+    console.log('Task completed: ' + this.name);
+    this.completed = true;
+};
+Task.prototype.save = function () {
+    console.log('Saving task: ' + this.name);
+};
 var task1 = new Task('This is a constructor task.');
 var task2 = new Task('This is a module task.');
 var task3 = new Task('This is a singleton task.');
