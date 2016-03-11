b6b111cbb5b580aa5f03610663b45c7a709a1a3c
AjayDec <ajaydec78@gmail.com>
2016-03-11 10:58:22 -0800

    JS: object inheritance via Object.create

--- task.js
+++ task.js
@@ -1,24 +1,33 @@
 //Adding attributes and properties to object
 
-var task1 = {};
+var task = {};
 
-task1.title = 'My title';
-task1.description = 'My description';
+task.title = 'My title';
+task.description = 'My description';
 
-Object.defineProperty(task1, 'toString', {
+Object.defineProperty(task, 'toString', {
     value: function () {
         return this.title + ', ' + this.description;
     },
     writable: false,
     enumerable: false,
-    configureable: true
+    configureable: false
 });
 
 
-console.log(Object.keys(task1)); // does not show 'toString' due to enumerable=false
+console.log(Object.keys(task)); // does not show 'toString' due to enumerable=false
 
-console.log(task1); // results in dumping Object attributes
+var urgentTask = Object.create(task);
 
-task1.toString = 'Hello, breaking code'; // this is silently rejected due to writable : false
+Object.defineProperty(urgentTask, 'toString', {
+    value: function () {
+        return this.title + ', I am urgent task';
+    },
+    writable: false,
+    enumerable: false,
+    configureable: false
+});
 
-console.log(task1.toString());
+console.log(Object.keys(urgentTask)); // does not show 'toString' due to enumerable=false
+
+console.log(urgentTask.toString());
