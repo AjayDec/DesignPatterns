bf6133e807a4f85483d354d299e8003b6f93cfa6
AjayDec <ajaydec78@gmail.com>
2016-03-11 20:05:38 -0800

    Module Pattern: Step 1

---
+++ taskRepository.js
@@ -0,0 +1,8 @@
+//Revealing module pattern [module is an Object literal, it contains set of related methods]
+
+var repo = function () {
+    return {
+        get: function () {},
+        set: function () {}
+    }
+};
