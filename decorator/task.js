08e796724ac02ceb7c649c4eadf15318aae580b8
AjayDec <ajaydec78@gmail.com>
2016-03-12 22:28:46 -0800

    Structural Patterns: Decorator Pattern (Step 2: Decorate the sample object with new attaributes and functionality)

--- decorator/task.js
+++ decorator/task.js
@@ -16,3 +16,19 @@ var myTask = new Task({
 });
 myTask.complete();
 myTask.save();
+
+var urgentTask = new Task({
+    name: 'Urgent Task'
+});
+urgentTask.priority = 'medium'; //decorate with additional attribute
+urgentTask.notify = function () { //decorate with additional method
+    console.log('notifying important people');
+
+};
+urgentTask.complete();
+urgentTask.save = function () { //decorate via modifing existing function
+    this.notify(); //new method
+    Task.prototype.save.call(this); //call existing method
+};
+
+urgentTask.save();
