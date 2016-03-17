f0597d5adf78e4226ccbf1e4e006345a25bf641a
AjayDec <ajaydec78@gmail.com>
2016-03-16 19:30:32 -0700

    Behavioral Patterns: Mediator Pattern (Step 2. Implement mediator)

--- mediator/main.js
+++ mediator/main.js
@@ -21,6 +21,38 @@ var auditingService = function () {
     };
 };
 
+var mediator = (function () {
+    var channels = {};
+
+    var subscribe = function (channel, context, func) {
+        if (!this.channels[channel]) {
+            this.channels[channel] = []
+        }
+        this.channels[channel].push({
+            context: context,
+            func: func
+        });
+    };
+
+    var publish = function (channel) {
+        if (!this.channels[channel]) {
+            return false;
+        }
+        var args = Array.prototype.slice.call(arguments, 1);
+        for (var i = 0; i < this.channels[channel].length; i++) {
+            var sub = this.channels[channel][i];
+            sub.func.apply(sub.context, args);
+
+        }
+    };
+
+    return {
+        channels: channels,
+        subscribe: subscribe,
+        publish: publish
+    }
+}());
+
 var task1 = new Task({
     name: 'A demo for mediator',
     user: 'jay'
@@ -29,5 +61,14 @@ var task1 = new Task({
 var not = new notificationService();
 var ls = new loggingService();
 var audit = new auditingService();
+
+mediator.subscribe('complete', not, not.update);
+mediator.subscribe('complete', ls, ls.update);
+mediator.subscribe('complete', audit, audit.update);
+
+task1.complete = function () {
+    mediator.publish('complete', this);
+    Task.prototype.complete.call(this);
+}
 
 task1.complete();
