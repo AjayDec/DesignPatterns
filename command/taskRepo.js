42a2c0361e1fa3b87b8c71075820f4597f1d633f
AjayDec <ajaydec78@gmail.com>
2016-03-17 07:55:16 -0700

    Behavioral Patterns: Command Pattern (Step 4. Implement command replay use case)

--- command/taskRepo.js
+++ command/taskRepo.js
@@ -1,26 +1,43 @@
 var repo = (function () {
-    var tasks = {};
-    var commands = [];
+    this.tasks = {};
+    this.commands = [];
     var get = function (id) {
         console.log('Getting for db.. taskId: ' + id);
-        if (tasks[id]) {
-            return tasks[id];
+        if (this.tasks[id]) {
+            return this.tasks[id];
         }
         return false;
     };
 
     var set = function (task) {
-        tasks[task.id] = task;
+        this.tasks[task.id] = task;
         console.log('Saving task in db.. ' + task.name);
+    };
+
+    var replay = function () {
+        for (var i = 0; i < this.commands.length; i++) {
+            var command = this.commands[i];
+            repo.executeNoLog(command.name, command.obj); // call this method to avoid infinte loop
+        }
     };
 
     return {
         get: get,
         set: set,
-        tasks: tasks,
-        commands: commands
+        tasks: this.tasks,
+        commands: this.commands,
+        replay: replay
     };
 })();
+
+repo.executeNoLog = function (name) {
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    if (repo[name]) {
+        return repo[name].apply(repo, args);
+    }
+    return false;
+};
 
 repo.execute = function (name) {
     var args = Array.prototype.slice.call(arguments, 1);
@@ -29,7 +46,7 @@ repo.execute = function (name) {
         obj: args[0]
     });
     if (repo[name]) {
-        return repo[name].apply(name, args);
+        return repo[name].apply(repo, args);
     }
     return false;
 };
@@ -57,4 +74,8 @@ repo.execute('set', {
     completed: false
 });
 
-console.log(repo.tasks);
+console.log('\n', repo.tasks);
+repo.tasks = {};
+console.log('\n', repo.tasks);
+repo.replay();
+console.log('\n', repo.tasks);
