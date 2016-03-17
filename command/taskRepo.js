90446955ec83b29ab2fa4b9b4176ec4994a29a09
AjayDec <ajaydec78@gmail.com>
2016-03-16 21:15:39 -0700

    Behavioral Patterns: Command Pattern (Step 3. Adding commands repository)

--- command/taskRepo.js
+++ command/taskRepo.js
@@ -1,22 +1,33 @@
 var repo = (function () {
+    var tasks = {};
+    var commands = [];
     var get = function (id) {
         console.log('Getting for db.. taskId: ' + id);
-        return {
-            name: 'I am a task ' + id + ' from db'
-        };
+        if (tasks[id]) {
+            return tasks[id];
+        }
+        return false;
     };
 
     var set = function (task) {
+        tasks[task.id] = task;
         console.log('Saving task in db.. ' + task.name);
     };
+
     return {
         get: get,
-        set: set
+        set: set,
+        tasks: tasks,
+        commands: commands
     };
 })();
 
 repo.execute = function (name) {
     var args = Array.prototype.slice.call(arguments, 1);
+    repo.commands.push({
+        name: name,
+        obj: args[0]
+    });
     if (repo[name]) {
         return repo[name].apply(name, args);
     }
@@ -30,3 +41,20 @@ repo.execute('set', {
     name: 'Task 1',
     completed: false
 });
+repo.execute('set', {
+    id: 2,
+    name: 'Task 2',
+    completed: false
+});
+repo.execute('set', {
+    id: 3,
+    name: 'Task 3',
+    completed: false
+});
+repo.execute('set', {
+    id: 4,
+    name: 'Task 4',
+    completed: false
+});
+
+console.log(repo.tasks);
