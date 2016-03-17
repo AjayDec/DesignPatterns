17d973557e191ce332f028e35b288438495e6a7e
AjayDec <ajaydec78@gmail.com>
2016-03-16 17:45:46 -0700

    Behavioral Patterns: Observer Pattern (Step 4. Implementing Subject)

--- observer/main.js
+++ observer/main.js
@@ -21,6 +21,31 @@ ObserverList.prototype.get = function (index) {
     }
 };
 
+ObserverList.prototype.count = function () {
+    return this.observerlist.length;
+};
+
+var ObservableTask = function (data) {
+    Task.call(this, data);
+    this.observers = new ObserverList();
+};
+
+ObservableTask.prototype.addObserver = function (observer) {
+    this.observers.add(observer);
+};
+
+ObservableTask.prototype.notify = function (context) {
+    var observersCount = this.observers.count();
+    for (var i = 0; i < observersCount; i++) {
+        this.observers.get(i)(context);
+    }
+};
+
+ObservableTask.prototype.save = function () {
+    this.notify(this);
+    Task.prototype.save.call(this);
+};
+
 var loggingService = function () {
     var message = 'Logging ';
     this.update = function (task) {
@@ -35,7 +60,7 @@ var auditingService = function () {
     };
 };
 
-var task1 = new Task({
+var task1 = new ObservableTask({
     name: 'A demo for constructor',
     user: 'jay'
 });
@@ -44,5 +69,8 @@ var not = new notificationService();
 var ls = new loggingService();
 var audit = new auditingService();
 
-task1.complete();
+task1.addObserver(not.update);
+task1.addObserver(ls.update);
+task1.addObserver(audit.update);
+
 task1.save();
